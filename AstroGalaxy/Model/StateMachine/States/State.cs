4b5175e23a425e6eecfb88844e420e109a642ef1
Vitaliy <vitaliy-efimov111@mail.ru>
2023-05-01 18:45:18 +0500

    Add StateMachine and States

---
+++ AstroGalaxy/Model/StateMachine/States/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace AstroGalaxy.Model.StateMachine.States;
+
+public abstract class State
+{
+    protected readonly GraphicsDeviceManager Graphics;
+
+    protected State(GraphicsDeviceManager graphics) => Graphics = graphics;
+
+    public event Action ChangeState;
+    
+    public abstract void Initialize();
+
+    public abstract void Update(GameTime gameTime);
+
+    public abstract void Draw(GameTime gameTime);
+
+    public void OnChangeState() => ChangeState?.Invoke();
+}
--- AstroGalaxy/View/GameUIRender.cs
+++ AstroGalaxy/View/GameUIRender.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using AstroGalaxy.Model.StateMachine.States;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.Entities.Systems;
 
@@ -8,23 +9,25 @@ public class GameUiRender : DrawSystem
 {
     private readonly SpriteBatch _spriteBatch;
     private readonly Texture2D _heartTexture;
+    private readonly MainGame _game;
 
-    public GameUiRender(GraphicsDevice graphicsDevice)
+    public GameUiRender(GraphicsDevice graphicsDevice, MainGame game)
     {
         _spriteBatch = new SpriteBatch(graphicsDevice);
         _heartTexture = AstroGalaxy.Instance.Content.Load<Texture2D>(Constants.HeartTexturePath);
+        _game = game;
     }
 
     public override void Draw(GameTime gameTime)
     {
         _spriteBatch.Begin();
-        
-        var game = AstroGalaxy.Instance;
-        var startY = game.Graphics.PreferredBackBufferHeight / 2 - game.Player.Health * Constants.HeartSpriteSize / 2;
 
-        for (var i = 0; i < game.Player.Health; i++)
+        var startY = AstroGalaxy.Instance.Graphics.PreferredBackBufferHeight / 2 -
+                     _game.Player.Health * Constants.HeartSpriteSize / 2;
+
+        for (var i = 0; i < _game.Player.Health; i++)
             _spriteBatch.Draw(_heartTexture, new Vector2(0, startY + i * Constants.HeartSpriteSize), Color.White);
-        
+
         _spriteBatch.End();
     }
 }
